89622425b34bb04ad42fa4592bf61bddd66fa8c0
Dora <kawaiidora@outlook.com>
2021-04-25 11:44:41 +0800

    ver 0.2 (bug exists)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # chrom_win_darkscrollbar
 Simply change the color of Chromium browser scrollbar on Windows platform by inject a meta tag.
+
+当网页应用暗色模式后，在windows系统和chromium浏览器搭配下，帮助忘记适配暗色滚动条的网页进行适配。
+
+[Greasy Fork](https://greasyfork.org/zh-CN/scripts/425503-chrom-win-darkscrollbar)
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name           chrom_win_darkscrollbar
+// @name:zh-CN     win上的chromium浏览器，暗色模式时让滚动条也变成暗色
 // @namespace      https://github.com/kawaiidora
 // @match          *://*/*
 // @grant          none
-// @version        0.1
+// @version        0.2
 // @author         Dora
 // @updateURL      https://raw.githubusercontent.com/kawaiidora/chrom_win_darkscrollbar/main/script.js
 // @downloadURL    https://raw.githubusercontent.com/kawaiidora/chrom_win_darkscrollbar/main/script.js
 // @homepageurl    https://github.com/kawaiidora/chrom_win_darkscrollbar
 // @supporturl     https://github.com/kawaiidora/chrom_win_darkscrollbar/issues
-// @run-at         document-start
+// @run-at         document-body
 // @description    Just change the color of the scrollbar when website applies dark theme but forgots the scrollbar. Works on Windows Chromium-based browser. 
 // @description:zh 作用在Win上的Chromium浏览器，当网页应用暗色模式却忘了把滚动条也变成暗色时，帮它变暗，不改变滚动条外观。
 // ==/UserScript==
